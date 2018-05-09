c95c601495d7bafaf584321345312a04b4819a0b
br3ndonland <brendon.w.smith@gmail.com>
2018-05-09 13:29:15 -0400

    Style page and map

--- info/map-methods.md
+++ info/map-methods.md
@@ -18,15 +18,14 @@ br3ndonland
 - [Prep](#prep)
   - [Directory setup](#directory-setup)
   - [JavaScript background](#javascript-background)
+- [Page layout](#page-layout)
 - [Google Maps API](#google-maps-api)
   - [Map](#map)
   - [Markers](#markers)
-  - [Drawing](#drawing)
+  - [Location list](#location-list)
 - [Knockout](#knockout)
 
 ## Prep
-
-The Full Stack Web Developer Nanodegree program (FSND) doesn't provide introductory training for JavaScript, so I started off taking several days to strengthen my JavaScript skills.
 
 ### Directory setup
 
@@ -35,6 +34,8 @@ The Full Stack Web Developer Nanodegree program (FSND) doesn't provide introduct
 - In the top-level directory, I added the README.md, and index.html. I got the HTML started by using the `html:5` Emmet snippet in vscode (just save the file as HTML, and type html).
 
 ### JavaScript background
+
+The Full Stack Web Developer Nanodegree program (FSND) doesn't provide introductory training for JavaScript, so I started off taking several days to strengthen my JavaScript skills.
 
 #### JavaScript syntax
 
@@ -58,6 +59,12 @@ The Full Stack Web Developer Nanodegree program (FSND) doesn't provide introduct
 - [x] [Firefox debugger playground](https://mozilladevelopers.github.io/playground/debugger/)
 - [ ] [Wes Bos JavaScript30](https://javascript30.com/) lesson 9, "14 must-know dev tools tricks"
 
+## Page layout
+
+- This is a simple one-page web app. Based on the mockup in the project description, I should have a header bar at the top, and a sliding drawer menu on the side.
+- The header was simple: just add HTML `<header>` tags before the `<div id="map"></div>`
+- I used an `<aside>` element for the sliding drawer menu. *TODO*
+
 ## Google Maps API
 
 ### Map
@@ -68,13 +75,37 @@ The Full Stack Web Developer Nanodegree program (FSND) doesn't provide introduct
 - [Set Map options](https://developers.google.com/maps/documentation/javascript/tutorial#MapOptions)
   - center
   - zoom
+  - style: I used the [Map Styler tool](https://mapstyle.withgoogle.com/) and the [styling instructions in the Google Maps JavaScript API docs](https://developers.google.com/maps/documentation/javascript/styling).
   - I kept the JavaScript in *index.js*.
   - The map requires CSS with `height` attributes for `body` and `#map`.
   - I attempted to change `var map` to `const map`, but wasn't able to get it to work.
+  - I was thinking about using the drawing tools to draw a border around Back Bay, but I expanded my list of coffee shops to the greater Boston area, so I didn't need the Back Bay border.
 
 ### Markers
 
-### Drawing
+- I added a simple drop animation for the marker using the [marker instructions in the Google Maps API docs](https://developers.google.com/maps/documentation/javascript/markers).
+- Changing the marker color is, strangely, not straightforward. TODO
+
+### Location list
+
+Rather than manually input an array, I decided to create a list in Gooogle Maps, ["Boston's best beans by br3ndonland"](https://goo.gl/maps/wmHnqRoFfhz), and access the JSON from that. This way, if the list is updated (if I add more coffee shops, for example), the app will automatically have the new information.
+
+I curated a list of my favorite coffee shops in the Boston area.
+
+- Barrington Coffee Roasters
+- Blue Bottle Coffee Harvard Square
+- Curio Coffee
+- George Howell Coffee
+- Gracenote Coffee
+- Intelligentsia Coffeebar Watertown
+- Neighborhoods Coffee and Crepes
+- Pavement Coffeehouse Fenway
+- Pavement Coffeehouse Newbury St
+- Render Coffee South End
+- Thinking Cup Newbury St
+- Thinking Cup Tremont St
+
+Filter the list by `open_now: true` from the JSON
 
 ## Knockout
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,234 @@
 // Google maps JavaScript
-// Initialize the map
+// Initialize map
 var map
 // Create a new blank array for all the listing markers.
 var markers = []
 function initMap () {
-  // Constructor creates a new map - only center and zoom are required.
+  // Create map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 42.3492897, lng: -71.0905478},
-    zoom: 13
+    zoom: 13,
+    styles: [
+      {
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#f5f5f5'
+          }
+        ]
+      },
+      {
+        'elementType': 'labels.icon',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#616161'
+          }
+        ]
+      },
+      {
+        'elementType': 'labels.text.stroke',
+        'stylers': [
+          {
+            'color': '#f5f5f5'
+          }
+        ]
+      },
+      {
+        'featureType': 'administrative.land_parcel',
+        'elementType': 'labels',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'featureType': 'administrative.land_parcel',
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#bdbdbd'
+          }
+        ]
+      },
+      {
+        'featureType': 'poi',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#eeeeee'
+          }
+        ]
+      },
+      {
+        'featureType': 'poi',
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#757575'
+          }
+        ]
+      },
+      {
+        'featureType': 'poi.business',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'featureType': 'poi.park',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#e5e5e5'
+          }
+        ]
+      },
+      {
+        'featureType': 'poi.park',
+        'elementType': 'labels.text',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'featureType': 'poi.park',
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#9e9e9e'
+          }
+        ]
+      },
+      {
+        'featureType': 'road',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#ffffff'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.arterial',
+        'elementType': 'labels',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.arterial',
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#757575'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.highway',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#dadada'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.highway',
+        'elementType': 'labels',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.highway',
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#616161'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.local',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.local',
+        'elementType': 'labels',
+        'stylers': [
+          {
+            'visibility': 'off'
+          }
+        ]
+      },
+      {
+        'featureType': 'road.local',
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#9e9e9e'
+          }
+        ]
+      },
+      {
+        'featureType': 'transit.line',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#e5e5e5'
+          }
+        ]
+      },
+      {
+        'featureType': 'transit.station',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#eeeeee'
+          }
+        ]
+      },
+      {
+        'featureType': 'water',
+        'elementType': 'geometry',
+        'stylers': [
+          {
+            'color': '#c9c9c9'
+          }
+        ]
+      },
+      {
+        'featureType': 'water',
+        'elementType': 'labels.text.fill',
+        'stylers': [
+          {
+            'color': '#9e9e9e'
+          }
+        ]
+      }
+    ]
   })
   var backBay = {lat: 42.3492897, lng: -71.0905478}
   var marker = new google.maps.Marker({
@@ -16,4 +237,8 @@ function initMap () {
     title: 'Back Bay',
     animation: google.maps.Animation.DROP
   })
+// TODO: Loop over locations to create markers
+// push markers
+// adjust bounds of map
+// info window
 }
